cb9370ff1a6b3d98c27d83bf326dd06fc7c6c44e
Eric Groppe Lepore Marques <37644388+gumami10@users.noreply.github.com>
2019-01-21 19:03:02 -0200

    Changed the order of the object

--- src/ContactCard.js
+++ src/ContactCard.js
@@ -4,8 +4,8 @@ function ContactCard(props) {
   console.log(props);
   return (
     <div className="contact-card">
-      <img src={props.contact.imgUrl} />
       <h3>{props.contact.name}</h3>
+      <img src={props.contact.imgUrl} />
       <p>Phone: {props.contact.phone}</p>
       <p>Email: {props.contact.email}</p>
     </div>
