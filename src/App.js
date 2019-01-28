da6a13aac35aca209dd00f5ed554dbdb0dafb03e
Eric Groppe Lepore Marques <37644388+gumami10@users.noreply.github.com>
2019-01-28 21:47:04 -0200

    To do App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
-import productsData from "./Products";
-import Product from "./ProductComponent";
+import TodoItem from "./TodoItem";
+import todoData from "./todoData";
 
-export default function App() {
-  const ProductMap = productsData.map(product => {
-    return (
-      <Product
-        key={product.id}
-        name={product.name}
-        price={product.price}
-        description={product.description}
-      />
-    );
-  });
-  return <div>{ProductMap}</div>;
+function App() {
+  const TodoItemMap = todoData.map(item => (
+    <TodoItem key={item.id} items={item} />
+  ));
+  return <div className="todo-list">{TodoItemMap}</div>;
 }
+
+export default App;
